660316545ca5fa90f0a2fe3ca1724bb0f641528e
Aikien Abasi <aikien4christ@yahoo.com>
2021-02-06 10:48:26 +0100

    finalise media query debugging, target screen set to 736px at landscape

--- aiksoft_js.js
+++ aiksoft_js.js
@@ -13,7 +13,7 @@ function loadHome() {
 
     $(window).resize(function () {
         var width = $(window).width();
-        if (width > 640) {
+        if (width > 736) {
             navBar.style.display = "block";
         } else {
             hideNavbar();
